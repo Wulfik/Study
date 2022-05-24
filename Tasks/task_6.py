f1f083b8b4ac620ce4562a96d96ae23ddb917b55
Wulfik <wulfik1999@gmail.com>
2022-05-24 15:49:37 +0300

    Add files via upload

--- Tasks/task_6.py
+++ Tasks/task_6.py
@@ -7,24 +7,24 @@ def solution(A):
                 if j == 0:
                     if A[i][j + 1] != A[i][j]:
                         amount += 1
-                elif j == 1:
+                elif j in range(1,len(A[i])-1):
                     if A[i][j + 1] != A[i][j]:
                         amount += 1
-                elif j == 2:
+                elif j == len(A[i])-1:
                     amount += 1
             else:
                 if j==0:
                     if A[i+1][j]!=A[i][j] and A[i][j+1]!=A[i][j]:
                         amount+=1
-                elif j==1:
+                elif j in range(1,len(A[i])-1):
                     if A[i+1][j]!=A[i][j] and A[i][j+1]!=A[i][j]:
                         amount+=1
-                elif j==2:
+                elif j == len(A[i])-1:
                     if A[i+1][j]!=A[i][j]:
                         amount+=1
             print(f'amount = {amount}')
     return amount
 
 
-A = [[5,4,4],[4,3,4],[3,2,4],[2,2,2],[3,3,4],[1,4,4,],[4,1,1],[1,4,4]]
+A = [[5,4,4,3],[4,3,4,3],[3,2,4,3],[2,2,2,3],[3,3,4,3],[1,4,4,3],[4,1,1,3],[1,4,4,2]]
 print(f'\namount = {solution(A)}')
